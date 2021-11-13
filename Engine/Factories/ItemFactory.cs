07e0c9083e16410a0c31d0cd8356a2cf1343f5cc
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2021-11-13 18:03:09 +0100

    05.1: Creating the Game Item Factory

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,22 @@
+﻿using Engine.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static readonly List<GameItem> _standardGameItems = new List<GameItem>();
+
+        static ItemFactory()
+        {
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+        }
+
+        public static GameItem CreateGameItem(int itemTypeId)
+        {
+            return _standardGameItems.FirstOrDefault(item => item.ItemTypeId == itemTypeId)?.Clone();
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,20 @@
+﻿namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        public Weapon(int itemTypeId, string name, int price, int minimumDamage, int maximumDamage)
+            : base(itemTypeId, name, price)
+        {
+            MinimumDamage = minimumDamage;
+            MaximumDamage = maximumDamage;
+        }
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeId, Name, Price, MinimumDamage, MaximumDamage);
+        }
+    }
+}
