1aa458cd52240519ea2c5321016ce0ecc6ee6916
ArjanKamphuis <arjan_kamphuis@hotmail.com>
2021-11-17 17:14:08 +0100

    12.6: Refactoring after adding Actions

--- Engine/Actions/AttackWithWeapon.cs
+++ Engine/Actions/AttackWithWeapon.cs
@@ -3,19 +3,17 @@ using System;
 
 namespace Engine.Actions
 {
-    public class AttackWithWeapon : IAction
+    public class AttackWithWeapon : BaseAction
     {
-        private readonly GameItem _weapon;
         private readonly int _minimumDamage;
         private readonly int _maximumDamage;
 
-        public event EventHandler<string> OnActionPerformed;
-
-        public AttackWithWeapon(GameItem weapon, int minimumDamage, int maximumDamage)
+        public AttackWithWeapon(GameItem itemInUse, int minimumDamage, int maximumDamage)
+            : base(itemInUse)
         {
-            if (weapon.Category != GameItem.ItemCategory.Weapon)
+            if (itemInUse.Category != GameItem.ItemCategory.Weapon)
             {
-                throw new ArgumentException($"{weapon.Name} is not a weapon");
+                throw new ArgumentException($"{itemInUse.Name} is not a weapon");
             }
             if (_minimumDamage < 0)
             {
@@ -26,12 +24,11 @@ namespace Engine.Actions
                 throw new ArgumentException("maximumDamage must be >= minimumDamage");
             }
 
-            _weapon = weapon;
             _minimumDamage = minimumDamage;
             _maximumDamage = maximumDamage;
         }
 
-        public void Execute(LivingEntity actor, LivingEntity target)
+        public override void Execute(LivingEntity actor, LivingEntity target)
         {
             string actorName = actor is Player ? "You" : $"The {actor.Name.ToLower()}";
             string targetName = target is Player ? "you" : $"the {target.Name.ToLower()}";
@@ -46,11 +43,6 @@ namespace Engine.Actions
                 ReportResult($"{actorName} hit {targetName} for {damage} points.");
                 target.TakeDamage(damage);
             }
-        }
-
-        private void ReportResult(string result)
-        {
-            OnActionPerformed?.Invoke(this, result);
         }
     }
 }
---
+++ Engine/Actions/BaseAction.cs
@@ -0,0 +1,24 @@
+﻿using Engine.Models;
+using System;
+
+namespace Engine.Actions
+{
+    public abstract class BaseAction : IAction
+    {
+        public event EventHandler<string> OnActionPerformed;
+
+        protected readonly GameItem _itemInUse;
+
+        protected BaseAction(GameItem itemInUse)
+        {
+            _itemInUse = itemInUse;
+        }
+
+        public abstract void Execute(LivingEntity actor, LivingEntity target);
+
+        protected void ReportResult(string result)
+        {
+            OnActionPerformed?.Invoke(this, result);
+        }
+    }
+}
